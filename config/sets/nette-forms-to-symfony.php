eec65b8e309ce8190b0e7a4e63f242731f4ad37a
TomasVotruba <tomas.vot@gmail.com>
2021-05-05 22:14:57 +0200

    add SetList

--- config/sets/nette-forms-to-symfony.php
+++ config/sets/nette-forms-to-symfony.php
@@ -2,16 +2,10 @@
 
 declare(strict_types=1);
 
+use Rector\NetteToSymfony\Rector\MethodCall\NetteFormToSymfonyFormRector;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
     $services = $containerConfigurator->services();
-
-    $services->defaults()
-        ->public()
-        ->autowire()
-        ->autoconfigure();
-
-    $services->load('Rector\\NetteToSymfony\\', __DIR__ . '/../src')
-        ->exclude([__DIR__ . '/../src/ValueObject']);
+    $services->set(NetteFormToSymfonyFormRector::class);
 };
