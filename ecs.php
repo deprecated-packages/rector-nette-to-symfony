f98bfab753fe2b4856a6fb68c6999d4b77f1014f
TomasVotruba <tomas.vot@gmail.com>
2021-05-06 22:57:37 +0200

    [ECS] use import directly

--- ecs.php
+++ ecs.php
@@ -7,15 +7,12 @@ use Symplify\EasyCodingStandard\ValueObject\Option;
 use Symplify\EasyCodingStandard\ValueObject\Set\SetList;
 
 return static function (ContainerConfigurator $containerConfigurator): void {
+    $containerConfigurator->import(SetList::COMMON);
+    $containerConfigurator->import(SetList::PSR_12);
+
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
-
-    $parameters->set(Option::SETS, [
-       SetList::COMMON,
-       SetList::PSR_12,
-    ]);
 };
-
